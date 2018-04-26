2cae548de10e94df03e9b780b1bbd263b0b304af
Jiří Filip <j.f.ilip@seznam.cz>
2018-04-26 16:50:35 +0200

    creating pypi package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pyarc',
   packages = ['pyarc', "pyarc.data_structures", "pyarc.algorithms"],
-  version = '1.0.5',
+  version = '1.0.8',
   description = 'An implementation of CBA algorithm',
   author = 'Jiří Filip',
   author_email = 'j.f.ilip@seznam.cz',
