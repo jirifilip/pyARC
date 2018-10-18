45b35eca6f0b07c5a511bd77e072b6afa38f5b35
Jiří Filip <j.f.ilip@seznam.cz>
2018-10-18 11:22:36 +0200

    class for fitting qcba implemented

--- pyarc/qcba/qcba.py
+++ pyarc/qcba/qcba.py
@@ -1,4 +1,40 @@
+from .data_structures import QuantitativeCAR
+from .transformation import QCBATransformation
+from .classifier import QuantitativeClassifier
+
 class QCBA:
 
-    def __init__(self):
-        pass
+    def __init__(self, cba_rule_model, quantitative_dataset):
+        self.cba_rule_model = cba_rule_model
+        self.quantitative_dataset = quantitative_dataset
+
+        self.__quant_rules = [ QuantitativeCAR(r) for r in cba_rule_model.clf.rules ] 
+
+        self.qcba_transformation = QCBATransformation(quantitative_dataset)
+
+        self.clf = None
+
+    def fit(
+            self, 
+            refitting=True,
+            literal_pruning=True,
+            trimming=True,
+            extension=True,
+            overlap_pruning=True,
+            transaction_based_drop=True
+        ):
+
+        transformation_dict = {
+            "refitting": refitting,
+            "literal_pruning": literal_pruning,
+            "trimming": trimming,
+            "extension": extension,
+            "overlap_pruning": overlap_pruning,
+            "transaction_based_drop": transaction_based_drop
+        }
+
+        transformed_rules, default_class = self.qcba_transformation.transform(self.__quant_rules, transformation_dict)
+
+        self.clf = QuantitativeClassifier(transformed_rules, default_class)
+
+        return self.clf
--- pyarc/qcba/transformation.py
+++ pyarc/qcba/transformation.py
@@ -24,13 +24,45 @@ class QCBATransformation:
         self.overlap_pruner = RuleOverlapPruner(self.dataset)
 
 
-    def transform(self, rules):
-        refitted = self.refitter.transform(rules)
-        literal_pruned = self.literal_pruner.transform(refitted)
-        trimmed = self.trimmer.transform(literal_pruned)
-        extended = self.extender.transform(trimmed)
-        post_pruned, default_class = self.post_pruner.transform(extended)
-        overlap_pruned = self.overlap_pruner.transform(post_pruned, default_class, transaction_based=self.transaction_based_drop)
+    def transform(self, rules, transformation_dict={}):
+
+        if not transformation_dict:
+            print("applying all transformations")
+            refitted = self.refitter.transform(rules)
+            literal_pruned = self.literal_pruner.transform(refitted)
+            trimmed = self.trimmer.transform(literal_pruned)
+            extended = self.extender.transform(trimmed)
+            post_pruned, default_class = self.post_pruner.transform(extended)
+            overlap_pruned = self.overlap_pruner.transform(post_pruned, default_class, transaction_based=self.transaction_based_drop)
+        
+        else:
+            print("applying selected transformations")
+            transformed_rules = rules
+
+            if transformation_dict.get("refitting", False):
+                print("refitting")
+                transformed_rules = self.refitter.transform(transformed_rules)
+            if transformation_dict.get("literal_pruning", False):
+                print("literal pruning")
+                transformed_rules = self.literal_pruner.transform(transformed_rules)
+            if transformation_dict.get("trimming", False):
+                print("trimming")
+                transformed_rules = self.trimmer.transform(transformed_rules)
+            if transformation_dict.get("extension", False):
+                print("extending")
+                transformed_rules = self.extender.transform(transformed_rules)
+
+            print("post pruning")
+            transformed_rules, default_class = self.post_pruner.transform(transformed_rules)
+            
+            if transformation_dict.get("overlap_pruning", False):
+                print("overlap pruning")
+                transaction_based = transformation_dict["transaction_based_drop"]
+
+                transformed_rules = self.overlap_pruner.transform(transformed_rules, default_class, transaction_based=transaction_based)
+
+            return transformed_rules, default_class
+
 
         return overlap_pruned, default_class
 
