ba3a7bbfcfc28be55c1b4780112930eb38606784
Jiří Filip <j.f.ilip@seznam.cz>
2019-08-13 13:12:22 +0200

    pypi upload corrected

--- pyarc/__init__.py
+++ pyarc/__init__.py
@@ -1,2 +1,8 @@
+try:
+    import fim
+except Exception:
+    raise Exception("Before using pyARC, the fim package must be installed first." + 
+    " For installation guide, refer to http://www.borgelt.net/pyfim.html")
+
 from .CBA import *
 from .data_structures import TransactionDB
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup
+
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+"""
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+"""
+
 setup(
   name = 'pyarc',
   packages = ['pyarc', "pyarc.data_structures", "pyarc.algorithms", "pyarc.qcba", "pyarc.utils"],
-  version = '1.0.9',
+  version = '1.0.15',
   description = 'An implementation of CBA algorithm',
+  #long_description=long_description,
+  #long_description_content_type='text/markdown',
   author = 'Jiří Filip',
   author_email = 'j.f.ilip@seznam.cz',
   url = 'https://github.com/jirifilip/pyARC',
   download_url = 'https://github.com/jirifilip/pyARC/archive/1.0.tar.gz',
-  keywords = ['classification', 'CBA', 'association rules', "machine learning"],
+  keywords = 'classification CBA association rules machine learning',
   classifiers = [],
-  install_requires=['pandas']
+  install_requires=['pandas', 'numpy', 'sklearn']
 )
