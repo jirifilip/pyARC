a88ff41148e55febb77d9dcba1737f856e671e0e
Jiří Filip <j.f.ilip@seznam.cz>
2019-08-13 13:18:25 +0200

    semaphore CI conf

--- pyarc/__init__.py
+++ pyarc/__init__.py
@@ -2,7 +2,7 @@ try:
     import fim
 except Exception:
     raise Exception("Before using pyARC, the fim package must be installed first." + 
-    " For installation guide, refer to http://www.borgelt.net/pyfim.html")
+    " For installation guide, please refer to http://www.borgelt.net/pyfim.html or https://pyfim.readthedocs.io/en/latest/source/install.html")
 
 from .CBA import *
 from .data_structures import TransactionDB
