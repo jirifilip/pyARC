89f5f6b5216614c04503f9f74158d799cab1b953
Jiří Filip <j.f.ilip@seznam.cz>
2019-08-18 16:26:55 +0200

    import errors corrected

--- pyarc/__init__.py
+++ pyarc/__init__.py
@@ -7,5 +7,4 @@ spec = importlib.util.find_spec(package_name)
 if spec is None:
     raise Exception("Before using pyARC, the 'fim' package must be installed first. For installation guide, please refer to http://www.borgelt.net/pyfim.html.")
 
-
 from .cba import CBA
